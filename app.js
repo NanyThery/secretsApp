fc46eecee013922059b6ef54b9cfff07250c1230
Nadine Thery <nadinethery@gmail.com>
2019-07-12 17:31:26 +0200

    final posting service implemented

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const session= require ("express-session");
-const passport = require ("passport");
-const passportLocalMongoose= require ("passport-local-mongoose");
-const GoogleStrategy = require ("passport-google-oauth20").Strategy;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require('mongoose-findorcreate');
-const FacebookStrategy = require ("passport-facebook").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 
 
 
@@ -48,14 +48,15 @@ mongoose.set("useCreateIndex", true);
 
 
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 });
 
-userSchema.plugin (passportLocalMongoose); // allows Passport to hash and salt passwords in the DB.
+userSchema.plugin(passportLocalMongoose); // allows Passport to hash and salt passwords in the DB.
 userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
@@ -96,9 +97,11 @@ passport.use(new FacebookStrategy({
         clientID: process.env.CLIENT_ID_FACEBOOK,
         clientSecret: process.env.CLIENT_SECRET_FACEBOOK,
         callbackURL: "http://localhost:3000/auth/facebook/secrets",
-     },
+    },
     function (accessToken, refreshToken, profile, done) {
-        User.findOrCreate({facebookId: profile.id}, function (err, user) {
+        User.findOrCreate({
+            facebookId: profile.id
+        }, function (err, user) {
             if (err) {
                 return done(err);
             }
@@ -110,11 +113,13 @@ passport.use(new FacebookStrategy({
 
 //Routing
 
-app.get("/", function (req, res){
- res.render("home");
+app.get("/", function (req, res) {
+    res.render("home");
 });
-app.get("/auth/google", 
-passport.authenticate("google", {scope: ["profile"]})
+app.get("/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile"]
+    })
 );
 
 app.get("/auth/facebook", passport.authenticate("facebook"));
@@ -125,63 +130,103 @@ app.get("/login", function (req, res) {
 
 });
 
-app.get("/auth/google/secrets", passport.authenticate("google", {failureRedirect:"/login"}), function (req, res) {
+app.get("/auth/google/secrets", passport.authenticate("google", {
+    failureRedirect: "/login"
+}), function (req, res) {
     res.redirect("/secrets");
 
 });
 
-app.get("/auth/facebook/secrets", passport.authenticate("facebook", {successRedirect: "/secrets", failureRedirect: "/login"}));
+app.get("/auth/facebook/secrets", passport.authenticate("facebook", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login"
+}));
 
 app.get("/register", function (req, res) {
     res.render("register");
 });
 
 app.get("/secrets", function (req, res) {
-    if(req.isAuthenticated()){
-    res.render("secrets");
+    User.find({"secret": {$ne: null }}, function (err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", {
+                    userWithSecret: foundUsers
+                });
+            }
+        }
+    });
+
+});
+
+app.get("/submit", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
 });
 
-app.get("/logout", function(req,res){
-req.logout();
-res.redirect("/");
-});
- 
-app.post("/register", function(req, res){
- User.register({username: req.body.username}, req.body.password,  function (err, user){
-     if (err) {
-         console.log(err);
-        res.redirect("/register");
-     } else {
-         passport.authenticate("local") (req, res, function() {
-             res.redirect("/secrets");
-         });
-     }
- });// This method comes from the Mongoose Passport.
 
-
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
 });
 
-app.post("/login", function(req,res){
-const user = new User ({
-    username: req.body.username,
-    password: req.body.password
+app.post("/register", function (req, res) {
+    User.register({
+        username: req.body.username
+    }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
+    }); // This method comes from the Mongoose Passport.
+
+
 });
 
-req.login(user, function (err){
-    if (err){
-        console.log (err);
-
-    }else{
-        passport.authenticate("local") (req, res, function() {
-            res.redirect("/secrets");
-        });
-    }
-        
+app.post("/submit", function (req, res) {
+    const sentSecret = req.body.secret;
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = sentSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
 });
-    
+
+
+app.post("/login", function (req, res) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
+
+    });
+
 });
 
 
